8b72a75b0bf054a1f2e66a566fea3c9f5ad0ad31
Josh-py <88326067+Josh-py@users.noreply.github.com>
2021-08-02 16:36:50 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,6 +2,9 @@ num1 = float(input('enter your first number: '))
 operator = input('enter operator: ')
 num2 = float(input('enter your second number: '))
 
+def error_prompt():
+    print('invalid operator')
+
 if operator == '+':
     print(num1 + num2)
 elif operator == '-':
@@ -11,4 +14,4 @@ elif operator == '*':
 elif operator == '/':
     print(num1 / num2)
 else:
-    print('invalid operator')
+    error_prompt()
