ffe772f741de4d30179faf77db9474d70dcf5ea4
Joseph-developer1 <77621910+Joseph-developer1@users.noreply.github.com>
2021-08-03 18:56:05 +0300

    added a loop

--- calculator.py
+++ calculator.py
@@ -1,27 +1,37 @@
 from math import *
-print('')
-print('BPC (c)')
+print('******************')
+print('┌──┐ ┌───┐┌───┐')
+print('│┌┐│ │┌─┐││┌─┐│')
+print('│└┘└┐│└─┘│││ └┘')
+print('│┌─┐││┌──┘││ ┌┐')
+print('│└─┘│││   │└─┘│')
+print('└───┘└┘   └───┘')
+print('*****************')
 print('')
 print('+, -, *, /, ^')
+print('')
 
-num1 = float(input('enter your first number: '))
-operator = input('enter operator: ')
-num2 = float(input('enter your second number: '))
+BPC = 1
 
+while BPC <= 1000000000000000000000000:  # yep! thats a septilion.
+    num1 = float(input('enter your first number: '))
+    operator = input('enter operator: ')
+    num2 = float(input('enter your second number: '))
 
-def error_prompt():
-    print('invalid operator')
+    def error_prompt():
+        print('invalid operator')
 
+    if operator == '+':
+        print(num1 + num2)
+    elif operator == '-':
+        print(num1 - num2)
+    elif operator == '*':
+        print(num1 * num2)
+    elif operator == '/':
+        print(num1 / num2)
+    elif operator == '^':
+        print(pow(num1, num2))
+    else:
+        error_prompt()
 
-if operator == '+':
-    print(num1 + num2)
-elif operator == '-':
-    print(num1 - num2)
-elif operator == '*':
-    print(num1 * num2)
-elif operator == '/':
-    print(num1 / num2)
-elif operator == '^':
-    print(pow(num1, num2))
-else:
-    error_prompt()
+    BPC += 1
