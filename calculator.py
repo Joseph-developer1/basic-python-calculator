d137497f57078f2be02752f5af47e729e7409635
Joseph-developer1 <77621910+Joseph-developer1@users.noreply.github.com>
2021-08-03 12:27:48 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,17 @@
+from math import *
+print('')
+print('BPC (c)')
+print('')
+print('+, -, *, /, ^')
+
 num1 = float(input('enter your first number: '))
 operator = input('enter operator: ')
 num2 = float(input('enter your second number: '))
 
+
 def error_prompt():
     print('invalid operator')
+
 
 if operator == '+':
     print(num1 + num2)
@@ -13,5 +21,7 @@ elif operator == '*':
     print(num1 * num2)
 elif operator == '/':
     print(num1 / num2)
+elif operator == '^':
+    print(pow(num1, num2))
 else:
     error_prompt()
